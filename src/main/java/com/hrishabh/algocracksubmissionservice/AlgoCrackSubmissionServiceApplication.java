4db4dfc0850d5d654697dfda8ce425b4082d17c6
Hrishabh Joshi <hrishabhjoshi123@gmail.com>
2025-07-29 15:36:00 +0530

    Project created

---
+++ src/main/java/com/hrishabh/algocracksubmissionservice/AlgoCrackSubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hrishabh.algocracksubmissionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoCrackSubmissionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoCrackSubmissionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrishabh/algocracksubmissionservice/AlgoCrackSubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrishabh.algocracksubmissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoCrackSubmissionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
