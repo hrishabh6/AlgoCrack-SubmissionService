8c4ca53edf7688b2c7968eadcfc820b295237d87
Hrishabh Joshi <hrishabhjoshi123@gmail.com>
2025-08-13 13:40:54 +0530

    Made some production changes

--- src/main/java/com/hrishabh/algocracksubmissionservice/consumer/SubmissionConsumer.java
+++ src/main/java/com/hrishabh/algocracksubmissionservice/consumer/SubmissionConsumer.java
@@ -3,18 +3,17 @@ package com.hrishabh.algocracksubmissionservice.consumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hrishabh.algocracksubmissionservice.dto.SubmissionDto;
-    import com.hrishabh.algocracksubmissionservice.service.ExternalCodeRunnerService;
+import com.hrishabh.algocracksubmissionservice.service.CodeRunnerService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SubmissionConsumer {
 
-//    private final CodeRunnerService codeRunnerService;
-    private final ExternalCodeRunnerService codeRunnerService;
+    private final CodeRunnerService codeRunnerService;
     private final ObjectMapper objectMapper;
 
-    public SubmissionConsumer(ExternalCodeRunnerService codeRunnerService, ObjectMapper objectMapper) {
+    public SubmissionConsumer(CodeRunnerService codeRunnerService, ObjectMapper objectMapper) {
         this.codeRunnerService = codeRunnerService;
         this.objectMapper = objectMapper;
     }
@@ -24,7 +23,6 @@ public class SubmissionConsumer {
         try{
             // Convert JSON to DTO
             SubmissionDto dto = objectMapper.readValue(submissionJson, SubmissionDto.class);
-
             // 💡 Pass the DTO to the new service for processing
             codeRunnerService.processSubmission(dto);
         } catch (JsonProcessingException e) {
